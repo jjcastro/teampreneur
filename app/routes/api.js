dc51b2a8e30cfa8a79a446f2f94ee814b43dcf66
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-11-02 18:58:10 -0500

    vainas

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,7 +2,6 @@ var express    = require('express');
 var bodyParser = require('body-parser'); 	// get body-parser
 var query      = require('pg-query'); 
 
-
 var apiRouter = express.Router();
 
 // test route to make sure everything is working 
@@ -11,56 +10,7 @@ apiRouter.get('/', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
-
-
-// on routes that end in /users
-// ----------------------------------------------------
-apiRouter.route('/users')
-
-	// create a user (accessed at POST http://localhost:8080/users)
-	.post(function(req, res) {
-		
-		
-
-	})
-
-	// get all the users (accessed at GET http://localhost:8080/api/users)
-	.get(function(req, res) {
-
-		var sql = "select * from users";
-
-    query(sql, [], function(err, rows) {
-      if (err) return res.send(err);
-
-      res.json(rows);
-    });
-
-	});
-
-// req.params.user_id
-// on routes that end in /users/:user_id
-// ----------------------------------------------------
-apiRouter.route('/users/:user_id')
-
-	// get the user with that id
-	.get(function(req, res) {
-		
-
-
-	})
-
-	// update the user with this id
-	.put(function(req, res) {
-		
-
-
-	})
-
-	// delete the user with this id
-	.delete(function(req, res) {
-		
-
-
-	});
+apiRouter.use('/users', require('./users'));
+apiRouter.use('/projects', require('./projects'));
 
 module.exports = apiRouter;
---
+++ app/routes/projects.js
@@ -0,0 +1,58 @@
+var express    = require('express');
+var bodyParser = require('body-parser');  // get body-parser
+var query      = require('pg-query'); 
+
+var router = express.Router();
+
+
+// on routes that end in /projects
+// ----------------------------------------------------
+router.route('/')
+
+  // create a project (accessed at POST http://localhost:8080/projects)
+  .post(function(req, res) {
+    
+    
+
+  })
+
+  // get all the projects (accessed at GET http://localhost:8080/api/projects)
+  .get(function(req, res) {
+
+    var sql = "select * from projects";
+
+    query(sql, [], function(err, rows) {
+      if (err) return res.send(err);
+
+      res.json(rows);
+    });
+
+  });
+
+// req.params.project_id
+// on routes that end in /projects/:project_id
+// ----------------------------------------------------
+router.route('/:project_id')
+
+  // get the project with that id
+  .get(function(req, res) {
+    
+
+
+  })
+
+  // update the project with this id
+  .put(function(req, res) {
+    
+
+
+  })
+
+  // delete the project with this id
+  .delete(function(req, res) {
+    
+
+
+  });
+
+module.exports = router;
