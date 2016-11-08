c1d53c9dfdc020a554211946955c54a01a6bc7d1
jdfandino10 <jd.fandino10@uniandes.edu.co>
2016-11-08 18:38:00 -0500

    post y get de projects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -11,17 +11,29 @@ router.route('/:user_id/projects')
 
   // create a project (accessed at POST http://localhost:8080/projects)
   .post(function(req, res) {
-    
-    res.json({ object: req.params });
+    var user_id = req.params.user_id;
+    var project = req.body;
+    var sql = "insert into projects (owner, fixed, name, description) values ($1, $2, $3, $4) RETURNING *";
+
+    query(sql, [user_id, false, project.name, project.description], function(err, rows) {
+      if (err) return res.send(err);
+
+      sql = "update users set project=$1 where id=$2";
+      console.log(rows);
+      query(sql, [rows[0].id, user_id], function(err, rows) {
+          if (err) return res.send(err);
+      });
+      res.json(rows);
+    });
 
   })
 
   // get all the projects (accessed at GET http://localhost:8080/api/projects)
   .get(function(req, res) {
+    var user_id = req.params.user_id;
+    var sql = "select * from projects where owner = $1";
 
-    var sql = "select * from projects";
-
-    query(sql, [], function(err, rows) {
+    query(sql, [user_id], function(err, rows) {
       if (err) return res.send(err);
 
       res.json(rows);
