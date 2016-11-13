1da780a05a6746963340ad4d3b191d1e00355a13
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-11-13 17:18:58 -0500

    keywords for user

--- app/routes/keywords.js
+++ app/routes/keywords.js
@@ -29,6 +29,34 @@ router.route('/')
 
   });
 
+router.route('/user')
+  // create a user (accessed at POST http://localhost:8080/users)
+  .post(function(req, res) {
+    
+
+
+  })
+
+  // get all the users (accessed at GET http://localhost:8080/api/users)
+  .get(function(req, res) {
+
+    var sql = "select * from keywords k, user_keywords uk where k.id=uk.keyword_id and uk.user_id=$1";
+
+    query(sql, [req.decoded.id], function(err, rows) {
+      if (err) return res.send(err);
+      
+      res.json(rows);
+    });
+
+  })
+
+  // delete the user with this id
+  .delete(function(req, res) {
+    
+
+
+  });
+
 // req.params.user_id
 // on routes that end in /users/:user_id
 // ----------------------------------------------------
